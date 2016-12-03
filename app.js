3b5399809f7723d9336fa4bbfd793216434a1873
voidcase <dat13ili@student.lu.se>
2016-12-03 14:13:56 +0100

    try to add to firebase

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ var taginput = document.getElementById("taginput");
 var fireRef = firebase.database.ref();
 function addSeeker(){
 	//add user to firebase
+	fireRef.child("users").push().set({tags:[]});
+	alert("user added");
 }

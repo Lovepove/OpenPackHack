8e1a66c1898a39a7458f33db7065a439e18ae758
voidcase <dat13ili@student.lu.se>
2016-12-03 15:13:57 +0100

    moved stuff to user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var taginput = document.getElementById("taginput");
 var fireRef = firebase.database().ref();
 function addSeeker(){
 	//add user to firebase
-	fireRef.child("users").push({tags:[taginput.value.split(',')]});
+	fireRef.child("users").push({tags:taginput.value.split(',')});
 	alert("user added");
+	taginput.value = "";
 }

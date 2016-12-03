816b40cb919bdea1ad694b630f05b242b1aca593
Vilhelm Lundqvist <v.lundqvist94@gmail.com>
2016-12-03 20:16:19 +0100

    Cleaning, polishing and refactoring of js/worker.js

--- js/worker.js
+++ js/worker.js
@@ -1,12 +1,18 @@
 var provider = new firebase.auth.GoogleAuthProvider();
+var username = "";
+var currTagsParagraph = document.getElementById("currentTags").getElementsByTagName("p")[0];
 
 
 firebase.auth().onAuthStateChanged(function(user) {
 	if (user) {
+    	username = user.uid;
+    	console.log("UID: " + username);
 		var header = document.getElementById("greeting");
 		header.innerHTML = "Hello, " + user.displayName + ". Let's find you a job today.";
     // User is signed in.
 	} else {
+		console.log("user not signed in");
+		document.body.innerText = "";
 		firebase.auth().signInWithRedirect(provider);
     // No user is signed in.
   }
@@ -17,6 +23,10 @@ firebase.auth().onAuthStateChanged(function(user) {
 var taginput = document.getElementById("taginput");
 var fireRef = firebase.database().ref();
 
+function addWorkerSkill() {
+	//fireRef.child("workers/" + username + "/tags").push({});
+}
+
 function addWorker(){
 	//add user to firebase
 	fireRef.child("workers").push({tags:taginput.value.split(',')});
@@ -24,21 +34,32 @@ function addWorker(){
 	taginput.value = "";
 }
 
+function addWorkerTag(tag) {
+	var btn = document.createElement("button");
+	btn.type = "button"
+	btn.className = "btn btn-default";
+	btn.innerText = tag;
+	currTagsParagraph.appendChild(btn);
+
+}
+
 function loadUsersTags(username){
 	var currTagsParagraph = document.getElementById("currentTags").getElementsByTagName("p")[0];
 
-	var tags = fireRef.child('workers/' + username + "/tags");
-	tags.once('value', function(snapshot){
+	var tagsRef = fireRef.child('workers/' + username + "/tags");
+	tagsRef.once('value', function(snapshot){
 		snapshot.forEach(function(childSnapshot){
-			var tagData = childSnapshot.val();
-			var btn = document.createElement("button");
-			btn.type = "button"
-			btn.className = "btn btn-default";
-			btn.innerText = tagData;
-			currTagsParagraph.appendChild(btn);
+			addWorkerTag(childSnapshot.val());
 		});
+	});
+	tagsRef.on('child_added', function(snapshot){
+		var event = snapshot.val();
+		console.log("event: " + event);
+		console.log("event.key: " + event.key);
+		console.log("event.title: " + event.title);
+		console.log("event.content: " + event.content);
+		addWorkerTag("$$$")
 	});
 }
 
-var defUser = "-KY459HQWUPqLRU6Soim";
-loadUsersTags(defUser);
+loadUsersTags(username);

5dfa2cf51ad66176eb91790bc98b700fcdf2b068
voidcase <dat13ili@student.lu.se>
2016-12-03 14:51:31 +0100

    ADDING USERS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var taginput = document.getElementById("taginput");
-var fireRef = firebase.database.ref();
+var fireRef = firebase.database().ref();
 function addSeeker(){
 	//add user to firebase
-	fireRef.child("users").push().set({tags:[]});
+	fireRef.child("users").push({tags:[taginput.value.split(',')]});
 	alert("user added");
 }

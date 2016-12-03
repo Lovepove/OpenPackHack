fe954c8b42f03493942f82ba9b42359bcc989f31
voidcase <dat13ili@student.lu.se>
2016-12-03 18:44:52 +0100

    WORKER SEARCH

--- js/company.js
+++ js/company.js
@@ -1,11 +1,12 @@
 function matchingWorkers(tags){
 	fireRef.child('/workers/').once('value').then(function(snapshot){
 		var all = snapshot.val();
-		return all.keys().filter(function(x){
-			workertags = Array.values(all[x].tags);
+		matches = Object.keys(all).filter(function(x){
+			workertags = Object.values(all[x].tags);
 			return tags.every(function(searchtag){
-				workertags.includes(searchtag);
+				return workertags.includes(searchtag);
 			});
 		});
+		console.log(matches);
 	});
 }
